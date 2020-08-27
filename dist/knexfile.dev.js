c313f8ebcfa43c7be16d180dad84a9de176e822a
Hassan Farahani <farahani.h1360@gmail.com>
2020-08-27 12:02:57 -0700

    Add production connection for deploy

--- dist/knexfile.dev.js
+++ dist/knexfile.dev.js
@@ -5,5 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://hassan:galaxy@localhost/community-forum'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
